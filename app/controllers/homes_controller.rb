0ea587011d67085e187832c9cd8d617f7e115752
fr15ky3816 <shota.t.3816@gmail.com>
2021-06-27 20:33:41 +0000

    add bar-menu

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,10 +1,17 @@
 class HomesController < ApplicationController
   def top
-    products = Product.where(is_sold: false)
-    @products = products.where(is_active: true).order(updated_at: :desc)
+    products = Product.where(is_sold: false, is_active: true)
+    @products = products.order(updated_at: :desc)
     # いいねした商品
-    
-    @likes = current_customer.likes
+    if customer_signed_in?
+      @likes = current_customer.likes
+    end
+    @products_yasai = products.where(genre_id: 1)
+    @products_kudamono = products.where(genre_id: 2)
+    @products_kome_zakkoku = products.where(genre_id: 3)
+    @products_kakousyokuhin = products.where(genre_id: 4)
+    @products_inryou = products.where(genre_id: 5)
+    @products_sonota = products.where(genre_id: 6)
 
 
   end
@@ -13,3 +20,4 @@ class HomesController < ApplicationController
 
   end
 end
+
